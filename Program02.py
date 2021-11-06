48e99f666f7fd0f1191a1d084af8dfb5baa93d67
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-06 11:01:33 +0800

    Trial Function

---
+++ Program02.py
@@ -0,0 +1,5 @@
+def getuserInput ():
+    apple_F = int(input("Apples you want to buy: "))
+    orange_F = int(input("Oranges you want to buy: "))
+    totalprice_F = (apple_F*20) + (orange_F*25)
+    return apple_F, orange_F, totalprice_F
