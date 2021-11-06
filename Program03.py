5a00dd3b82350eb215de06f7081e6c6b02da019a
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-06 11:29:01 +0800

    Display Function

--- Program03.py
+++ Program03.py
@@ -4,3 +4,8 @@ def getuserInput ():
     totalapple_F = (balance_F/appleprice_F)
     change_F = (balance_F%appleprice_F)
     return balance_F, appleprice_F, totalapple_F, change_F
+
+def display(totalappleFunc, changeFunc):
+    print(f"You can buy {totalappleFunc} apples and your change is {changeFunc} pesos.")
+
+balanceF, applepriceF, totalappleF, changeF = getuserInput()
