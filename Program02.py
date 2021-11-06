bc898a8610a29880baebcdf4366a7a52b674648b
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-06 11:05:24 +0800

    Display Function

--- Program02.py
+++ Program02.py
@@ -3,3 +3,9 @@ def getuserInput ():
     orange_F = int(input("Oranges you want to buy: "))
     totalprice_F = (apple_F*20) + (orange_F*25)
     return apple_F, orange_F, totalprice_F
+
+def display(totalpriceFunc):
+    print(f"The total amount is {totalpriceFunc}")
+
+
+apple, orange, total = getuserInput()
