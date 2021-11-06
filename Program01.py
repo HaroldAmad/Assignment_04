387408ec496e7faf2a7cf23e1ca3370ca0755b8c
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-06 10:39:24 +0800

    Display Function

--- Program01.py
+++ Program01.py
@@ -3,3 +3,8 @@ def getuserInput ():
     age_F = int(input("Age: "))
     add_F = input("Address: ")
     return name_F, age_F, add_F
+
+def display(nameFunc, ageFunc, addFunc):
+    print(f"Hi, my name is {nameFunc}. I {ageFunc} years old and I live in {addFunc}.")
+
+name, age, add = getuserInput
