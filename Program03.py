7c735d84aecd945af9352de9014de86ea4dca8d7
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-06 11:31:59 +0800

    Final Commit with Integer Correction in the Total of Apples

--- Program03.py
+++ Program03.py
@@ -1,7 +1,7 @@
 def getuserInput ():
     balance_F = int(input("Amount of money: "))
     appleprice_F = int(input("Price of an apple: "))
-    totalapple_F = (balance_F/appleprice_F)
+    totalapple_F = int(balance_F/appleprice_F)
     change_F = (balance_F%appleprice_F)
     return balance_F, appleprice_F, totalapple_F, change_F
 
@@ -9,3 +9,4 @@ def display(totalappleFunc, changeFunc):
     print(f"You can buy {totalappleFunc} apples and your change is {changeFunc} pesos.")
 
 balanceF, applepriceF, totalappleF, changeF = getuserInput()
+display(totalappleF, changeF)
